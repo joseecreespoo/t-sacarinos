ddfc2dff5934f3170890373a85a39c77c7674983
tonicotitular <ajgarciarias@gmail.com>
2022-03-24 09:50:44 -0700

    Antonio José García Arias- Comienzo creacion de funciones utilizar LIBRERIA PEWEE PARA LA BD

---
+++ Python/Functions.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+class FuncionesParaElTrabajito :
+    #Funcion para coger los datos del csv que devuelve el objeto df "Es el array  bidimensional del csv "
+    def  gettingCSV():
+        #Leemos el csv para utilizar la librería pandas que lo convierte en tabla
+        df = pd.read_csv("../t-sacarinos/Python/vehiculos-2021.csv")
+        ##Devolvemos el array bidimensional
+        return df
+    print (gettingCSV())
+    lista= gettingCSV
+    def insertingDataToTheDataBase(lista):
+        
+        pass
+    
