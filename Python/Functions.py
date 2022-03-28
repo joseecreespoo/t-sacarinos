5e33098b927d8d3339333bfd14cd8f6b3ad9e30f
Antonio José García Arias <73525861+ajgarciarias10@users.noreply.github.com>
2022-03-28 12:54:19 +0200

    Antonio José García Arias QUEDAN COSITAS

--- Python/Functions.py
+++ Python/Functions.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pymysql
 import Functions as f
 class FuncionesParaElTrabajito :
+    #FUNCION QUE INSERTA EL CSV EN LA DATABASE
     def insertCsvToTheDatabse(connection,idCoche,tipoVehiculo,cantidad) : 
         # queries for inserting values
         try :
@@ -19,28 +20,88 @@ class FuncionesParaElTrabajito :
             #commiting the connection then closing it.
             connection.commit()
         print("Introduccion correcta con la base de datos")  
-    #Funcion para coger los datos del csv que devuelve el objeto df "Es el array  bidimensional del csv "
+    #Funcion para coger los datos del csv que devuelve el objeto df
     def  gettingCSV():
         #Leemos el csv para utilizar la librería pandas que lo convierte en tabla
         df = pd.read_csv("../t-sacarinos/Python/vehiculos-2021.csv")
         ##Devolvemos el array bidimensional
         return df
+    #Limpiar los datos de la lista y sacamos esos datos en tuplas
     def  extractDataFromArrayList(cursor,arrayconlasCosas) :
        tuples = [tuple(x) for x in arrayconlasCosas.values]
        for each in tuples:
             f.FuncionesParaElTrabajito.insertCsvToTheDatabse(cursor,each[0],str(each[1]),each[2])
-    def searchingINTOTHEDATABASE(cursor) :
+    #Borrado donde el id es : 
+    def deleteDataWhereIDinVehiculos(connection,idCoche):    
+        sql = "DELETE FROM sacarinosDB.vehiculos WHERE('%s')" % (idCoche)
+        curr = connection.cursor()
+        curr.execute(sql)
+    #CONSULTA DE LA TABLA ESPAÑA : 
+    def searchingINTOTHEDATABASE(connection) :
         try :
-            select1 = f"SELECT * FROM sacarinosDB.españa"
+            select1 = "SELECT * FROM sacarinosDB.espana"
             #executing the quires
-            cursor.execute(select1)
-            rows = cursor.fetchall()
+            curr = connection.cursor()
+            curr.execute(select1)
+            rows = curr.fetchall()
             for row in rows:
                 print(row)   
-            cursor.commit()
+            connection.commit()
         except pymysql.Error as e : 
             print("ERROR AL BUSCAR")
             #commiting the connection then closing it.
-            cursor.commit()
+            connection.commit()
         print("Busqueda correcta con la base de datos")
-        
+    
+    def menu():
+        print("Welcome to T-SACARINOS CONSULTING")
+        menu_options = {
+            1:"WATCH ALL THE DATA FROM THE TABLE SPAIN" ,
+            2:"WATCH ALL THE DATA FROM THE TABLE  VEHICLES",
+            3:"INTRODUCE DATA TO SPAIN",
+            4:"INTRODUCE DATA TO VEHICLES",
+            5:"WIPE OUT DATA FROM SPAIN WHERE ID",
+            6: "WIPE OUT DATA FROM VEHICLES WHERE ID",
+            7: "SEARCH DATA FROM VEHICLES WHERE ENROLLMENT(ID OF THE CAR)",
+            8: "SEARCH DATA FROM SPAIN WHERE ENROLLMENT(ID OF THE CAR)"
+        }
+        def print_menu():
+            for key in menu_options.keys():
+                print (key, '--', menu_options[key] )
+        if __name__=='__main__':
+            while(True):
+                print_menu()
+                option = ''
+                try:
+                    option = int(input('Enter your choice: '))
+                except:
+                    print('Wrong input. Please enter a number ...')
+                #Check what choice was entered and act accordingly
+                if option == 1:
+                    
+                elif option == 2:
+                    option2()
+                elif option == 3:
+                    option3()
+                elif option == 4:
+                    print('Thanks message before exiting')
+                    exit()
+                else:
+                    print('Invalid option. Please enter a number between 1 and 4.')
+    
+        choises={
+           
+        }
+        for i in choises.keys(): # Loop to print all Choises and key of choise ! 
+            print(f"{i} - {choises[i]}")
+        arg=int(input("Pleasse Chose : "))
+        R=choises.get(arg,-1)
+        while R==-1:
+            print("\n Wrong Choise ! Try again ....\n")
+            menu()
+        else:
+            print(f"You Chosed {R}")
+            if R==1:
+                login()
+            if R==2:
+                register()   
--- Python/connectionbd.py
+++ Python/connectionbd.py
@@ -8,11 +8,17 @@ class connectionbd :
             #Conectamos la base de datos a mi servidor local xamp
             connection = pymysql.connect(host="localhost", user="root", passwd="", database="sacarinosDB")
             cursor = connection.cursor()
+            ##ESTO SE HACE SOLO 1 VEZ QUE ES PARA METER EL CSV EN LA TABLA ESPAÑA
             #Obtenemos un arraybidimnesional de las cosas del csv
-            arrayConLasCositasXD = f.FuncionesParaElTrabajito.gettingCSV()
-            f.FuncionesParaElTrabajito.extractDataFromArrayList(connection,arrayConLasCositasXD)
+            ##arrayConLasCositasXD = f.FuncionesParaElTrabajito.gettingCSV()
+            ##f.FuncionesParaElTrabajito.extractDataFromArrayList(connection,arrayConLasCositasXD)
+            f.FuncionesParaElTrabajito.searchingINTOTHEDATABASE(connection)
+            print("WELCOME TO T-SACARINOS DATABASE")
+
+ 
         except pymysql.Error as e:
                print("No se puede conectar a la base de datos")
+        print("Se conectó")
         
 
         
