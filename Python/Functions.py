86ee50b4aeb0810b31e48aeff5265fcb340f9b54
Antonio José García Arias <73525861+ajgarciarias10@users.noreply.github.com>
2022-03-28 12:14:31 +0200

    Antonio José García Arias INSERCCION CSV DONE

--- Python/Functions.py
+++ Python/Functions.py
@@ -1,33 +1,34 @@
 import pandas as pd
 import pymysql
+import Functions as f
 class FuncionesParaElTrabajito :
+    def insertCsvToTheDatabse(connection,idCoche,tipoVehiculo,cantidad) : 
+        # queries for inserting values
+        try :
+            print(idCoche)
+            print(tipoVehiculo)
+            print(cantidad)
+            insert1 = ("INSERT INTO sacarinosDB.espana (id,tipoVehiculo,cantidad) VALUES ('%s','%s','%s')" % (idCoche,tipoVehiculo,cantidad))
+            #executing the quires
+            curr = connection.cursor()
+            curr.execute(insert1)
+            connection.commit()
+
+        except pymysql.Error as e : 
+            print("ERROR AL INSERTAR")
+            #commiting the connection then closing it.
+            connection.commit()
+        print("Introduccion correcta con la base de datos")  
     #Funcion para coger los datos del csv que devuelve el objeto df "Es el array  bidimensional del csv "
     def  gettingCSV():
         #Leemos el csv para utilizar la librería pandas que lo convierte en tabla
         df = pd.read_csv("../t-sacarinos/Python/vehiculos-2021.csv")
         ##Devolvemos el array bidimensional
         return df
-    def  extractDataFromArrayList(dato) :
-        for fila in dato :
-            print(fila)
-        return fila
- 
-       
-            
-    def insertCsvToTheDatabse(cursor,idCoche,tipoVehiculo,cantidad) : 
-        # queries for inserting values
-        try :
-            insert1 = f"INSERT INTO sacarinosDB.espana (idCoche,tipoVehiculo,cantidad) VALUES ({idCoche},{tipoVehiculo},{cantidad})"
-            #executing the quires
-            cursor.execute(insert1)
-            cursor.commit()
-            cursor.close()
-        except pymysql.Error as e : 
-            print("ERROR AL INSERTAR")
-            #commiting the connection then closing it.
-            cursor.commit()
-            cursor.close()
-        print("Introduccion correcta con la base de datos")  
+    def  extractDataFromArrayList(cursor,arrayconlasCosas) :
+       tuples = [tuple(x) for x in arrayconlasCosas.values]
+       for each in tuples:
+            f.FuncionesParaElTrabajito.insertCsvToTheDatabse(cursor,each[0],str(each[1]),each[2])
     def searchingINTOTHEDATABASE(cursor) :
         try :
             select1 = f"SELECT * FROM sacarinosDB.españa"
--- Python/connectionbd.py
+++ Python/connectionbd.py
@@ -3,32 +3,16 @@ import pymysql
 import sys
 import Functions as f
 class connectionbd : 
-        #Obtenemos un arraybidimnesional de las cosas del csv
-        arrayConLasCositasXD = f.FuncionesParaElTrabajito.gettingCSV()
-        #Sacamos el id de Coche y los demas atributos a utilizar sin limpiar ya que obtenemos un dato DATAFRAME
-        idCocheNoClean = arrayConLasCositasXD['_id'].to_list()
-        tipoVehiculoNoClean = arrayConLasCositasXD['TIPO DE VEHÍCULO'].to_list()
-        cantidadNoClean = arrayConLasCositasXD['CANTIDAD'].to_list()
         try:
             #database connection
             #Conectamos la base de datos a mi servidor local xamp
             connection = pymysql.connect(host="localhost", user="root", passwd="", database="sacarinosDB")
             cursor = connection.cursor()
-            try:
-                #LIMPIAMOS LOS DATOS
-                idCoche = f.FuncionesParaElTrabajito.extractDataFromArrayList(idCocheNoClean)
-                print ("----------")
-                tipoVehiculo =  f.FuncionesParaElTrabajito.extractDataFromArrayList(tipoVehiculoNoClean)
-                print ("----------")
-                cantidad =  f.FuncionesParaElTrabajito.extractDataFromArrayList(cantidadNoClean)
-                print ("----------")
-                f.FuncionesParaElTrabajito.insertCsvToTheDatabse(cursor,idCoche,tipoVehiculo,cantidad)
-            except pymysql.Error as e:
-                    print("Ninguna de las dos") 
-            
+            #Obtenemos un arraybidimnesional de las cosas del csv
+            arrayConLasCositasXD = f.FuncionesParaElTrabajito.gettingCSV()
+            f.FuncionesParaElTrabajito.extractDataFromArrayList(connection,arrayConLasCositasXD)
         except pymysql.Error as e:
-                print("No se puede conectar a la base de datos")
-        print("Conexión correcta con la base de datos")
+               print("No se puede conectar a la base de datos")
         
 
         
