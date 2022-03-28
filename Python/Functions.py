c37941262aa312112a23f766b0a1fe6b2cbc1a18
Antonio José García Arias <73525861+ajgarciarias10@users.noreply.github.com>
2022-03-28 11:03:30 +0200

    TONI INSERTANDO LOS DATOS DEL CSV FALTA ELERROR DE LA CONCATENACION

--- Python/Functions.py
+++ Python/Functions.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import pymysql
 class FuncionesParaElTrabajito :
     #Funcion para coger los datos del csv que devuelve el objeto df "Es el array  bidimensional del csv "
     def  gettingCSV():
@@ -7,6 +7,50 @@ class FuncionesParaElTrabajito :
         df = pd.read_csv("../t-sacarinos/Python/vehiculos-2021.csv")
         ##Devolvemos el array bidimensional
         return df
-    print (gettingCSV())
-    lista= gettingCSV
-    
+    def  extractDataFromArrayList(dato) :
+        
+        s=""  
+        t=0
+        for fila1 in dato:
+            print (fila1)
+            if (isinstance(fila1, str)):
+                print("CADENA",type(fila1))
+                s+= " ' "+ fila1 + "'" + ","
+                dato = s
+            else:
+                print("INT",type(fila1))
+                t+= "'"+ fila1 + "'" + ","
+                dato = t
+        print (dato)  
+        return dato
+       
+            
+    def insertCsvToTheDatabse(cursor,idCoche,tipoVehiculo,cantidad) : 
+        # queries for inserting values
+        try :
+            insert1 = f"INSERT INTO sacarinosDB.espana (idCoche,tipoVehiculo,cantidad) VALUES ({idCoche},{tipoVehiculo},{cantidad})"
+            #executing the quires
+            cursor.execute(insert1)
+            cursor.commit()
+            cursor.close()
+        except pymysql.Error as e : 
+            print("ERROR AL INSERTAR")
+            #commiting the connection then closing it.
+            cursor.commit()
+            cursor.close()
+        print("Introduccion correcta con la base de datos")  
+    def searchingINTOTHEDATABASE(cursor) :
+        try :
+            select1 = f"SELECT * FROM sacarinosDB.españa"
+            #executing the quires
+            cursor.execute(select1)
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)   
+            cursor.commit()
+        except pymysql.Error as e : 
+            print("ERROR AL BUSCAR")
+            #commiting the connection then closing it.
+            cursor.commit()
+        print("Busqueda correcta con la base de datos")
+        
--- Python/connectionbd.py
+++ Python/connectionbd.py
@@ -3,37 +3,40 @@ import pymysql
 import sys
 import Functions as f
 class connectionbd : 
+        #Obtenemos un arraybidimnesional de las cosas del csv
+        arrayConLasCositasXD = f.FuncionesParaElTrabajito.gettingCSV()
+        #Sacamos el id de Coche y los demas atributos a utilizar sin limpiar ya que obtenemos un dato DATAFRAME
+        idCocheNoClean = arrayConLasCositasXD['_id'].to_list()
+        print(idCocheNoClean)
+        tipoVehiculoNoClean = arrayConLasCositasXD['TIPO DE VEHÍCULO'].to_list()
+        cantidadNoClean = arrayConLasCositasXD['CANTIDAD'].to_list()
         try:
             #database connection
             #Conectamos la base de datos a mi servidor local xamp
             connection = pymysql.connect(host="localhost", user="root", passwd="", database="sacarinosDB")
             cursor = connection.cursor()
-            # queries for inserting values
+            try:
+                #LIMPIAMOS LOS DATOS
+                idCoche = f.FuncionesParaElTrabajito.extractDataFromArrayList(idCocheNoClean)
+                print ("----------")
+                tipoVehiculo =  f.FuncionesParaElTrabajito.extractDataFromArrayList(tipoVehiculoNoClean)
+                print ("----------")
+                cantidad =  f.FuncionesParaElTrabajito.extractDataFromArrayList(cantidadNoClean)
+                print ("----------")
+                f.FuncionesParaElTrabajito.insertCsvToTheDatabse(cursor,idCoche,tipoVehiculo,cantidad)
+            except pymysql.Error as e:
+                    print("Ninguna de las dos") 
+            
         except pymysql.Error as e:
                 print("No se puede conectar a la base de datos")
-                sys.exit(1)
         print("Conexión correcta con la base de datos")
-        cursor = connection.cursor()
-        arrayConLasCositasXD = f.FuncionesParaElTrabajito.gettingCSV()
-        #Sacamos el id de Coche        
-        idCoche = arrayConLasCositasXD['_id']
-        tipoVehiculo = arrayConLasCositasXD['TIPO DE VEHÍCULO']
-        cantidad = arrayConLasCositasXD['CANTIDAD']  
         
+
         
-        def insertCsvToTheDatabse(cursor,idCoche,tipoVehiculo,cantidad) : 
-            # queries for inserting values
-            try :
-                insert1 = "INSERT INTO sacarinosDB.españa (idCoche,tipoVehiculo,cantidad) VALUES ({idCoche},{tipoVehiculo},{cantidad})"
-                #executing the quires
-                cursor.execute(insert1)
-                cursor.commit()
-            except pymysql.Error as e : 
-                sys.exit(1)
-                print("ERROR")
-                #commiting the connection then closing it.
-                cursor.commit()
-                cursor.close()
-        insertCsvToTheDatabse(cursor,idCoche,tipoVehiculo,cantidad)
-        
+
+       
+           
+      
+       
+
             
